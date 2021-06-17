4a81fc586d82b6dd82a5b431789627f29ded6aeb
prasad1909 <prasadiyer12345@gmail.com>
2021-06-17 20:15:49 +0530

    apis

--- Ecom/urls.py
+++ Ecom/urls.py
@@ -17,12 +17,60 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from shop.models import *
 from . import views
+
+from django.contrib.auth.models import User
+from rest_framework import routers, serializers, viewsets
+
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'   
+
+class FruitViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.filter(category='Fruits & Vegetables')
+    serializer_class = ProductSerializer
+
+class SnacksViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.filter(category='Snacks & Munchies')
+    serializer_class = ProductSerializer
+
+class DrinksViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.filter(category='Drinks & Beverages')
+    serializer_class = ProductSerializer
+
+class StaplesViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.filter(category='Daily Staples')
+    serializer_class = ProductSerializer
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'fruits', FruitViewSet)
+router.register(r'snacks', SnacksViewSet)
+router.register(r'drinks', DrinksViewSet)
+router.register(r'staples', StaplesViewSet)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('shop.urls')),
     path('', views.register),
     path('login/', views.loginPage),
-    path('logout/', views.logOutUser)
+    path('logout/', views.logOutUser),
+    path('api-auth/', include('rest_framework.urls')),
+    path('', include(router.urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
